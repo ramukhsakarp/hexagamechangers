436c9b49311f3c43d8dae7263c50c4bd43ee3839
ramukhsakarp <prakash.spec07@gmail.com>
2018-04-17 17:03:22 +0530

    add implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 //const API_KEY = require('./apiKey');
-
+var value = 200;
 var server = express()
 // app.get('/', function (req, res) { res.send('Hello World') })
 // app.listen(8080)
@@ -14,7 +14,28 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
-	
+	var mycustomresponse ="";
+	if(req.body.result.action == 'add'){
+		value = value + 10;
+        if(value<1000)  
+            mycustomresponse = value;
+		else 
+			mycustomresponse ="value should not be greater than 1000";
+		
+		return res.json({
+        speech: mycustomresponse ,
+        displayText: 'test',
+        source: 'get-movie-details'
+		});
+	}
+	else if(req.body.result.action == 'add'){
+		return res.json({
+        speech: req.body.result.action ,
+        displayText: req.body.result.action,
+        source: 'get-movie-details'
+    });
+	}
+	else
     return res.json({
         speech: req.body.result.action ,
         displayText: req.body.result.action,
@@ -27,6 +48,22 @@ server.post('/get-movie-details', (req, res) => {
         source: 'get-movie-details'
     });
 });
+//////////////////////////
+
+	server.get('/add', function(req, res) {	     
+        value = value + 10;
+        if(value<1000)  
+            res.send('Value :' + value);
+        else
+            res.status(400).send("value should not be greater than 1000");
+    });	
+    server.get('/remove', function(req, res) {	     
+        value = value - 10;
+        if(value>0)  
+            res.send('Value :' + value);
+        else
+            res.status(400).send("value should not be Less than 0");
+	});	
 
 server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
